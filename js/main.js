b056c66f515e7788f1cccb9f40dc5695cc216bc1
bigeppo <carlog91cast@gmail.com>
2022-06-30 15:56:37 +0200

    add Milestone 1

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=15"
         },
         "likes": 80,
-        "is_liked" : true,
+        "is_liked": true,
         "created": "2022-06-25"
     },
     {
@@ -20,7 +20,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=10"
         },
         "likes": 120,
-        "is_liked" : false,
+        "is_liked": false,
         "created": "2022-06-03"
     },
     {
@@ -32,7 +32,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=20"
         },
         "likes": 78,
-        "is_liked" : true,
+        "is_liked": true,
         "created": "2022-05-15"
     },
     {
@@ -44,7 +44,7 @@ const posts = [
             "image": null
         },
         "likes": 56,
-        "is_liked" : false,
+        "is_liked": false,
         "created": "2022-04-03"
     },
     {
@@ -56,7 +56,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=29"
         },
         "likes": 95,
-         "is_liked" : false,
+        "is_liked": false,
         "created": "2022-03-05"
     },
     {
@@ -68,7 +68,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=33"
         },
         "likes": 95,
-         "is_liked" : true,
+        "is_liked": true,
         "created": "2022-02-02"
     },
     {
@@ -80,7 +80,7 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=59"
         },
         "likes": 95,
-         "is_liked" : false,
+        "is_liked": false,
         "created": "2022-02-01"
     },
     {
@@ -92,7 +92,46 @@ const posts = [
             "image": "null"
         },
         "likes": 95,
-         "is_liked" : true,
+        "is_liked": true,
         "created": "2021-12-11"
     }
 ];
+
+// Milestone 1 -
+//  Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed, rimuovendo i contenuti statici.
+
+const insertPost = document.getElementById('container');
+posts.forEach(element => {
+    let createPost = document.createElement('div');
+    createPost.innerHTML = `<div class="post">
+    <div class="post__header">
+        <div class="post-meta">
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${element.author.image}" alt="Phil Mangione">
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${element.author.name}</div>
+                <div class="post-meta__time">${element.created}</div>
+            </div>
+        </div>
+    </div>
+    <div class="post__text">${element.content}</div>
+    <div class="post__image">
+        <img src="${element.media}" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+            </div>
+        </div>
+    </div>
+</div>`
+    insertPost.append(createPost);
+});
